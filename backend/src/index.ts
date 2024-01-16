c3889c8ba24c47302d598343093a372534375120
Sunny Sahsi <sahsisunny@gmail.com>
2024-01-16 05:41:31 +0530

    fox cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,7 +19,7 @@ const app = express()
 
 app.use(
    cors({
-      origin: process.env.CORS_ORIGIN,
+      origin: process.env.CORS_ORIGIN || /\.gittrackr\.engineer$/,
       credentials: true,
    }),
 )
